2ab8889ff7b569052362b69806412d2e6b6eded7
popobg <pauline.bouyssou1@gmail.com>
2024-10-29 15:00:32 +0100

    Proposed new solution ex4

--- src/main/java/ex4/DateUtils.java
+++ src/main/java/ex4/DateUtils.java
@@ -7,45 +7,13 @@ import java.util.Date;
  *
  */
 public class DateUtils {
-    /** pattern de formatage */
-    private SimpleDateFormat formateur;
 
-    /**
-     * Constructeur
-     * @return
-     */
-    public DateUtils() {
-        this.formateur = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");;
+    public static String format(String pattern, Date date) {
+        SimpleDateFormat formateur = new SimpleDateFormat(pattern);
+        return formateur.format(date);
     }
 
-    /**
-     * Constructeur
-     * @param pattern chaîne de caractères correspondant au pattern
-     *               de formatage de la date
-     * @return
-     */
-    public DateUtils(String pattern) {
-        this.formateur = new SimpleDateFormat(pattern);
-    }
-
-
-    public String format(Date date) {
-        return this.formateur.format(date);
-    }
-
-    /**
-     * Getter
-     * @return formateur
-     */
-    public SimpleDateFormat getFormateur() {
-        return formateur;
-    }
-
-    /**
-     * Setter
-     * @param formateur formateur
-     */
-    public void setFormateur(SimpleDateFormat formateur) {
-        this.formateur = formateur;
+    public static String formatDefaut(Date date) {
+        return format("dd/MM/yyyy HH:mm:ss", date);
     }
 }
